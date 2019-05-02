1c6cfe5f91b9176d776bcfc7dfaa94689054fd13
Ervin Jusufagic <ERVJUS@zacco.com>
2019-05-02 10:13:23 +0200

    fixed responsiveness on login component, changed buttons

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 
 import TextField from "@material-ui/core/TextField";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 import logo from "../resources/cinema-svg.svg";
 
 import { createApolloFetch } from "apollo-fetch";
@@ -11,7 +11,8 @@ import { connect } from "react-redux";
 import {
   handleEmail,
   handlePassword,
-  authenticate
+  authenticate,
+  signUp
 } from "../actions/loginActions";
 import "../css/Login.css";
 
@@ -52,38 +53,60 @@ class Login extends Component {
     }
     return (
       <div className="login">
-        <img src={logo} alt="" />
+        <div className="logoContainer">
+          <img className="logo" src={logo} alt="" />
+        </div>
         <p className="appName">WhatToWatch?</p>
 
-        <TextField
-          type="email"
-          value={this.props.email}
-          onChange={event => this.handleChange(event)}
-          id="standard-dense"
-          label="Email"
-          margin="dense"
-        />
-        <TextField
-          type="password"
-          id="standard-dense"
-          value={this.props.password}
-          onChange={event => this.handleChange(event)}
-          label="Password"
-          margin="dense"
-        />
-        <div className="buttonGroup">
-          <Button variant="contained" onClick={() => this.handleSubmit()}>
-            Login
-        </Button>
-          <Button variant="contained" onClick={() => this.props.history.push("/signup")}>
-            SignUp
-        </Button>
+        <div className="form">
+          <TextField
+            type="email"
+            value={this.props.email}
+            onChange={event => this.handleChange(event)}
+            id="standard-dense"
+            label="Email"
+            margin="dense"
+          />
+          <TextField
+            type="password"
+            id="standard-dense"
+            value={this.props.password}
+            onChange={event => this.handleChange(event)}
+            label="Password"
+            margin="dense"
+          />
         </div>
-
+        <div className="buttonGroup">
+          <Button
+            style={styles.login}
+            variant="contained"
+            onClick={() => this.handleSubmit()}
+          >
+            Login
+          </Button>
+          <Button
+            style={styles.signUp}
+            variant="contained"
+            onClick={() => this.props.history.push("/signup")}
+          >
+            SignUp
+          </Button>
+        </div>
       </div>
     );
   }
 }
+
+const styles = {
+  login: {
+    backgroundColor: "#2c1e5a",
+    color: "#dedede"
+  },
+  signUp: {
+    backgroundColor: "#dedede",
+    color: "#2c1e5a"
+  }
+};
 
 const mapDispatchToProps = {
   handleEmail,
