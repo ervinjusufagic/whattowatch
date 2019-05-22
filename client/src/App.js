70bbc5ddd7e1c930c1917f8d98c3a93056d91527
Ervin Jusufagic <ERVJUS@zacco.com>
2019-05-22 16:42:24 +0200

    fixed npm vulnerabilities, and removed unused dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
 import React, { Component } from "react";
 
 import Dashboard from "./components/Dashboard";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
 
 import { BrowserRouter } from "react-router-dom";
-
-const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
-});
 
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={client}>
-        <BrowserRouter>
-          <Dashboard />
-        </BrowserRouter>
-      </ApolloProvider>
+      <BrowserRouter>
+        <Dashboard />
+      </BrowserRouter>
     );
   }
 }
